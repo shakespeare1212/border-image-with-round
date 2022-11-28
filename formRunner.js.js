c16babc4dfb847c3d5e6ace51d9b5ef4614ed41e
James McLain <shakepeare@sbcglobal.net>
2022-11-27 20:35:23 -0800

    Add files via upload

---
+++ formRunner.js.js
@@ -0,0 +1,12 @@
+<script>
+function honest()
+{
+    alert("You are honest. Thanksgiving is great.");
+}
+
+function dishonest()
+{
+    alert("You are lying. Everyone loves Thanks giving.");
+    
+}
+</script>
